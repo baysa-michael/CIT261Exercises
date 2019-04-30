a5a7b46b5fc394d0fe83d65f9f7fddc7db3de86b
mb1 <bay15003@byui.edu>
2019-04-29 23:10:24 -0600

    Completed Chapter 2 Exercise for Chessboard

--- week2/chessboard/chessboard.js
+++ week2/chessboard/chessboard.js
@@ -1,7 +1,34 @@
 function startChessboard() {
+    // Get the chessboard size input by the user
+    let size = parseInt(document.getElementById("size").value);
 
+    // Confirm that a number was inputby the user
+    if (isNaN(size)) {
+        buildChessboard();
+    } else {
+        buildChessboard(size);
+    }
 }
 
 function buildChessboard(size = 1) {
+    // Output the chessboard to the Console
+    for (let i = 0; i < size; i++) {
+        // Build the string for the current line of the chessboard
+        let outputLine = "";
+        let isEven = (i % 2 === 0);
 
+        for (let j = 0; j < size; j++) {
+            if (isEven) {
+                outputLine += " "
+            } else {
+                outputLine += "#"
+            }
+
+            // Reverse the isEven Status
+            isEven = !isEven;
+        }
+
+        // Output the line to the console
+        console.log(outputLine);
+    }
 }
