07b8004d20b8d82af646babec11729c3db1eb26a
mb1 <bay15003@byui.edu>
2019-05-16 22:42:14 -0600

    Work on Week 4 Team Assignment

--- week4/week_4_team_assignment/jsTicTacToe.js
+++ week4/week_4_team_assignment/jsTicTacToe.js
@@ -3,7 +3,8 @@ window.addEventListener("load", resetBoard);
 
 
 function resetBoard() {
-    // Reset Player Turn
+    // Reset Victory Status and Player Turn
+    document.getElementById("victoryStatus").innerHTML = "";
     document.getElementById("playerTurn").innerHTML = "1";
 
     // Clear the current board
@@ -14,6 +15,7 @@ function resetBoard() {
 
     // Reconstruct the board
     let board = document.createElement("table");
+    board.id = "board";
     for (let i = 0; i < 3; i++) {
         // Construct Board Row
         let boardRow = document.createElement("tr");
@@ -51,12 +53,19 @@ function resetBoard() {
             cell.style.height = cellSize;
             cell.style.width = cellSize;
 
+            // Center the cell contents
+            cell.style.textAlign = "center";
+            cell.style.verticalAlign = "center";
+
+            // Size of the cell contents
+            cell.style.fontSize = "1em";
+
             // Add an ID and Class to the cell
-            cell.id = "Cell" + i + "-" + j;
+            cell.id = "Cell" + (i * 3 + j + 1);
             cell.classList.add("unselected");
 
-            // Add click event listeners to the cell
-            cell.addEventListener("click", event => {
+            // Add the event listeners
+            cell.addEventListener("click", () => {
                 selectCell(cell.id);
             });
 
@@ -75,20 +84,116 @@ function resetBoard() {
     boardContainer.appendChild(board);
 }
 
-function selectCellEventListener() {
+function selectCell(cellID) {
+    // Clear the message box
+    document.getElementById("message").innerHTML = "";
 
+    // Target the appropriate cell
+    let targetCell = document.getElementById(cellID);
+
+    // Determine if this cell has already been played by a player
+    if (!targetCell.classList.contains("unselected")) {
+        // If already played, inform the user and end the function
+        document.getElementById("message").innerHTML =
+            "<p>This cell has already been selected - please choose another cell.</p>";
+
+        return;
+    }
+
+    // Retrieve the player turn
+    let playerTurn = document.getElementById("playerTurn");
+    let currentPlayer = parseInt(playerTurn.innerHTML);
+
+    // Determine the player symbol
+    let playerSymbol = (currentPlayer === 1 ? "X" : "O");
+
+    // Add the appropriate symbol to the cell and the class list
+    targetCell.innerHTML = playerSymbol;
+    targetCell.classList.remove("unselected");
+    targetCell.classList.add(playerSymbol);
+
+    // Determine if player has won the game from last move or if there is a stalemate
+    if (checkVictory(playerSymbol, cellID)) {
+        // Display the victory status
+        document.getElementById("victoryStatus").innerHTML = "Player " + currentPlayer + " is the winner!";
+
+        // Remove the listeners and preserve the board
+        removeListeners()
+    } else if (checkStalemate()) {
+        // Display the victory status
+        document.getElementById("victoryStatus").innerHTML = "This round is a stalemate.";
+
+        // Remove the listeners and preserve the board
+        removeListeners()
+    } else {
+        // Update the player
+        playerTurn.innerHTML = (currentPlayer === 1 ? "2" : "1");
+    }
 }
 
-function selectCell(cellID) {
-    // Retrieve the player turn
-    let currentPlayer = parseInt(document.getElementById("playerTurn").value);
+function checkVictory(playerSymbol, cellID) {
+    // Assign the cell ID's
+    let cell1 = document.getElementById("Cell1");
+    let cell2 = document.getElementById("Cell2");
+    let cell3 = document.getElementById("Cell3");
+    let cell4 = document.getElementById("Cell4");
+    let cell5 = document.getElementById("Cell5");
+    let cell6 = document.getElementById("Cell6");
+    let cell7 = document.getElementById("Cell7");
+    let cell8 = document.getElementById("Cell8");
+    let cell9 = document.getElementById("Cell9");
 
-    // Add a canvas element to the cell
+    // Return if current player is victorious
+    switch (cellID) {
+        case "Cell1":
+            return ((cell2.classList.contains(playerSymbol) && cell3.classList.contains(playerSymbol)) ||
+                cell5.classList.contains(playerSymbol) && cell9.classList.contains(playerSymbol) ||
+                cell4.classList.contains(playerSymbol) && cell7.classList.contains(playerSymbol));
+        case "Cell2":
+            return ((cell1.classList.contains(playerSymbol) && cell3.classList.contains(playerSymbol)) ||
+                cell5.classList.contains(playerSymbol) && cell8.classList.contains(playerSymbol));
+        case "Cell3":
+            return ((cell1.classList.contains(playerSymbol) && cell2.classList.contains(playerSymbol)) ||
+                cell5.classList.contains(playerSymbol) && cell7.classList.contains(playerSymbol) ||
+                cell6.classList.contains(playerSymbol) && cell9.classList.contains(playerSymbol));
+        case "Cell4":
+            return ((cell1.classList.contains(playerSymbol) && cell7.classList.contains(playerSymbol)) ||
+                cell5.classList.contains(playerSymbol) && cell6.classList.contains(playerSymbol));
+        case "Cell5":
+            return ((cell1.classList.contains(playerSymbol) && cell9.classList.contains(playerSymbol)) ||
+                cell2.classList.contains(playerSymbol) && cell8.classList.contains(playerSymbol) ||
+                cell3.classList.contains(playerSymbol) && cell7.classList.contains(playerSymbol) ||
+                cell4.classList.contains(playerSymbol) && cell6.classList.contains(playerSymbol));
+        case "Cell6":
+            return ((cell3.classList.contains(playerSymbol) && cell9.classList.contains(playerSymbol)) ||
+                cell4.classList.contains(playerSymbol) && cell5.classList.contains(playerSymbol));
+        case "Cell7":
+            return ((cell1.classList.contains(playerSymbol) && cell4.classList.contains(playerSymbol)) ||
+                cell3.classList.contains(playerSymbol) && cell5.classList.contains(playerSymbol) ||
+                cell8.classList.contains(playerSymbol) && cell9.classList.contains(playerSymbol));
+        case "Cell8":
+            return ((cell7.classList.contains(playerSymbol) && cell9.classList.contains(playerSymbol)) ||
+                cell2.classList.contains(playerSymbol) && cell5.classList.contains(playerSymbol));
+        case "Cell9":
+            return ((cell7.classList.contains(playerSymbol) && cell8.classList.contains(playerSymbol)) ||
+                cell1.classList.contains(playerSymbol) && cell5.classList.contains(playerSymbol) ||
+                cell3.classList.contains(playerSymbol) && cell6.classList.contains(playerSymbol));
+    }
+}
 
+function checkStalemate() {
+    for (let i = 1; i <= 9; i++) {
+        if (document.getElementById("Cell" + i).classList.contains("unselected")) {
+            return false;
+        }
+    }
 
-    // Draw the appropriate symbol
+    return true;
+}
 
-
-    // Remove the current listener
-    document.getElementById(cellID).removeEventListener()
+function removeListeners() {
+    // Remove the listener elements on the board by cloning and replacing
+    let oldElement = document.getElementById("board");
+    let newElement = oldElement.cloneNode(true);
+    oldElement.parentNode.replaceChild(newElement, oldElement);
 }
